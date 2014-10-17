e06d4bf77f732b64813e29123c81360b0016ccde
simfeld <sml.imfeld@gmail.com>
2014-10-17 18:25:01 +0200

    modules folder

--- mod_rides/js/mod_rides_mootools.js
+++
@@ -1,9 +0,0 @@
-// load accordion when site first opened
-/*window.addEvent('domready', function() {
-		var acc = new Fx.Accordion('#accordion table.title', '#accordion .content');
-	});*/
-// accordion loader function
-function loadAcc()
-{
-  var acc = new Fx.Accordion('#accordion table.title', '#accordion .content');
-}
